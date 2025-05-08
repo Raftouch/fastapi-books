c77e0dccf504901808d9f900698ea26f5c961280
raftouch <rafanelkin@gmail.com>
2025-05-08 18:02:47 +0200

    connect to db

--- main.py
+++ main.py
@@ -1,49 +1,95 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Depends
 import uvicorn
 from pydantic import BaseModel
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+from typing import Annotated
+
 
 app = FastAPI()
 
-books = [
-    {"id": 1, "title": "The Midnight Library", "author": "Matt Haig"},
-    {"id": 2, "title": "Project Hail Mary", "author": "Andy Weir"},
-    {"id": 3, "title": "The Song of Achilles", "author": "Madeline Miller"},
-    {"id": 4, "title": "Klara and the Sun", "author": "Kazuo Ishiguro"}
-]
+engine = create_async_engine("sqlite+aiosqlite:///books.db")
+
+new_session = async_sessionmaker(engine, expire_on_commit=False)
 
 
-@app.get("/", tags=["Books 📚"], summary='Get all books')
-def get_all_books():
-    return books
+class Base(DeclarativeBase):
+    pass
 
+class BookDB(Base):
+    __tablename__ = "books"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str]
+    author: Mapped[str]
 
-@app.get("/{id}", tags=["Books 📚"], summary='Get one book')
-def get_book(id: int):
-    for book in books:
-        if book["id"] == id:
-            return book
-    raise HTTPException(status_code=404, detail=f"No book found for id {id}")
-
-
-class Book(BaseModel):
+class BookCreateSchema(BaseModel):
     title: str
     author: str
 
+class BookSchema(BookCreateSchema):
+    id: int
 
-@app.post("/", tags=["Books 📚"], summary='Add a book')
-def add_book(new_book: Book):
-    book_to_add = {"id": len(books) + 1, "title": new_book.title, "author": new_book.author}
-    books.append(book_to_add)
+    
+async def get_session():
+    async with new_session() as session:
+        yield session
+
+
+SessionDep = Annotated[AsyncSession, Depends(get_session)]
+
+
+@app.post("/setup_db", tags=["Setup DB 📁"])
+async def setup_database():
+    async with engine.begin() as connection:
+        await connection.run_sync(Base.metadata.drop_all)
+        await connection.run_sync(Base.metadata.create_all)
+    return {"success": True}
+
+
+@app.post("/books", tags=["Books 📚"], summary='Add a book')
+async def add_book(data: BookCreateSchema, session: SessionDep):
+    new_book = BookDB(
+        title=data.title, 
+        author=data.author
+    )
+    session.add(new_book)
+    await session.commit()
     return {"success": True, "message": "Book has been added"}
 
 
-@app.delete("/{id}", tags=["Books 📚"], summary="Delete a book")
-def remove_book(id: int):
-    for book in books:
-        if book["id"] == id:
-            books.remove(book)
-            return {"success": True, "message": "Book has been removed"}
-    raise HTTPException(status_code=404, detail=f"No book found for id {id}")
+@app.get("/books", tags=["Books 📚"], summary='Get all books')
+async def get_all_books(session: SessionDep):
+    query = select(BookDB)
+    result = await session.execute(query)
+    return result.scalars().all()
+
+
+@app.get("/books/{id}", tags=["Books 📚"], summary='Get one book')
+async def get_book(id: int, session: SessionDep):
+    query = select(BookDB).filter(BookDB.id == id)
+    result = await session.execute(query)
+    book = result.scalar_one_or_none()
+
+    if book is None:
+        raise HTTPException(status_code=404, detail=f"No book found for id {id}")
+    
+    return book
+
+
+@app.delete("/books/{id}", tags=["Books 📚"], summary="Delete a book")
+async def remove_book(id: int, session: SessionDep):
+    query = select(BookDB).filter(BookDB.id == id)
+    result = await session.execute(query)
+    book = result.scalar_one_or_none()
+
+    if book is None:
+        raise HTTPException(status_code=404, detail=f"No book found for id {id}")
+    
+    await session.delete(book)
+    await session.commit()
+
+    return {"success": True, "message": "Book has been removed"}
 
 
 if __name__ == "__main__":
